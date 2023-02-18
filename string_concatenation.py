fda8b6c50c3ad5b7f3d0bd9c3874799c2f4fa10d
YoungTchalla18 <jolatunj@gmail.com>
2023-02-18 09:41:31 -0500

    added comments for str.join in Python 3.5

--- string_concatenation.py
+++ string_concatenation.py
@@ -3,13 +3,13 @@ colors = ["red", "green", "purple", "blue", "yellow"]
 # instead of this
 s = colors[0]
 for color in colors[1:]:
-    s += f", {color}"
+    s += f", {color}"  # `s += ', ' + color` in Python 3.5 or lower
 print(s)
 
 # or this
 s = ""
 for color in colors:
-    s += f"{color}, "
+    s += f"{color}, "  # `s += color + ', '` in Python 3.5 or lower
 s = s[:-2]
 print(s)
 
